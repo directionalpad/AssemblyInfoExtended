0157872e27cecbaab2c0d93cbe6d132052a8fa42
Bailey Dickens <directionalpadd@gmail.com>
2016-09-18 02:18:30 -0400

    Updated AssemblyInfo AssemblyDescription field to be in line with the github project description.

--- AssemblyInfoExtended/Properties/AssemblyInfo.cs
+++ AssemblyInfoExtended/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AssemblyInfoExtended")]
-[assembly: AssemblyDescription("Extends the available assembly information classes in the System.Reflection namespace.")]
+[assembly: AssemblyDescription("AssemblyInfoExtended is a .NET library that provdes additional attributes for use in your applications AssemblyInfo.cs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AssemblyInfoExtended")]
