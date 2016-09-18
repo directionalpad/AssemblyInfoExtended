e7c6948d9eb61113bce0f9e20599b983f096b26e
Bailey Dickens <directionalpadd@gmail.com>
2016-09-18 00:02:23 -0400

    Initial project commit

---
+++ AssemblyInfoExtended/AssemblyCommitHash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace System.Reflection
+{
+    /// <summary>
+    /// Defines a commit hash attribute for the assembly manifest.
+    /// </summary>
+    public class AssemblyCommitHash
+    {
+        public string _hash;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyCommitHash"/> class with the commit hash of the assembly being attributed.
+        /// </summary>
+        /// <param name="hash">The commit hash of the attributed assembly.</param>
+        public AssemblyCommitHash(string hash) { _hash = hash;  }
+    }
+}
---
+++ AssemblyInfoExtended/AssemblyInternalVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace System.Reflection
+{
+    /// <summary>
+    /// Defines additional version information for the assembly manifest.
+    /// </summary>
+    public class AssemblyInternalVersion
+    {
+        public string _version;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyInternalVersion"/> class, specifying the internal file version.
+        /// </summary>
+        /// <param name="version">The internal version number of the attributed assembly.</param>
+        public AssemblyInternalVersion(string version) { _version = version;  }
+    }
+}
